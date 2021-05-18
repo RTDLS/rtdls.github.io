f9a7b67062e96749e2cce8b371ace7b28a39b09b
RTDLS <72914783+RTDLS@users.noreply.github.com>
2021-05-18 13:09:39 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBTVoUSKK2aorhtA4Jk5zKifO1qzZ2wMhw",
+    authDomain: "rtdls-615a9.firebaseapp.com",
+    databaseURL: "https://rtdls-615a9.firebaseio.com",
+    projectId: "rtdls-615a9",
+    storageBucket: "rtdls-615a9.appspot.com",
+    messagingSenderId: "754389936714",
+    appId: "1:754389936714:web:dfb5dff0a77a7312f654b3",
+    measurementId: "G-T3ZMZCQ3RM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
